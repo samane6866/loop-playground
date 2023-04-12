0442d4ab70ffa393a5cd9cd551e49be4410ba0c8
Oscar Miras <oscar.miras@gmail.com>
2023-04-12 10:06:31 +0200

    Create ex9b.js

---
+++ ex9b.js
@@ -0,0 +1,34 @@
+/**
+ * DIFÍCIL
+ * 
+ * LA palabra del día. Primero, intenta entender este algoritmo. ¿Qué es lo que hace? https://lapalabradeldia.com/
+ */
+
+/**
+ * Completa el bucle:
+ * 
+ * 1. Si la siguiente letra que ha introducido el usuario es la misma letra que la que tocaría en la palabra del día, mostramos por consola : "la letra {letra} es correcta"
+ * 2. Si la siguiente letra que ha introducido el usuario NO esta en la palabra del dia, mostramos por consola : "le letra {letra} NO es correcta"
+ * 3. Si la siguiente letra que ha introducido el usuario se encuentra en la palabra del dia, PERO no es la posición correcta, mostrar por consola: "la letra {letra} es correcta, PERO se encuentra en una posición diferente"
+ */
+
+let palabraDelDia= "bucle";
+let palabraIntroducida = "bueno";
+
+for (let i=0;i<palabraDelDia.length;i++) {
+
+    let siguienteLetraCorrecta = palabraDelDia[i];
+    let siguienteLetraIntroducida = palabraIntroducida[i];
+
+    // Tu código a partir de aquí, dentro del bucle
+}
+
+// Resultado esperado:
+
+/**
+ * "La letra b es correcta"
+ * "La letra u es correcta"
+ * "La letra e es correcta, pero se encuentra en una posición diferente"
+ * "La letra n NO es correcta"
+ * "LA letra o NO es correcta"
+ */
