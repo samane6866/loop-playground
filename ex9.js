d99445ea73eab4771e2ba5438b45fe1ca7cfc5a1
samane6866 <samane.bahramian@gmail.com>
2023-04-12 14:23:32 +0200

    hasta ex10 done

--- ex9.js
+++ ex9.js
@@ -2,14 +2,14 @@
  * El bucle infinito
  *
  * El siguiente bucle 'while', no acaba nunca. ¿Por qué?
- * 
+ *
  * Arreglalo para que la palabra "Ejecutar!" se muestre 10 veces por el terminal
  */
 
-let n=10;
-let i=0;
+let n = 10;
+let i = 0;
 
-while(i<n) {
-    console.log("Ejecutar!")
+while (i < n) {
+  console.log("Ejecutar!");
+  i = i + 1;
 }
-
--- ex9b.js
+++ ex9b.js
@@ -1,34 +1,40 @@
 /**
  * DIFÍCIL
- * 
+ *
  * LA palabra del día. Primero, intenta entender este algoritmo. ¿Qué es lo que hace? https://lapalabradeldia.com/
  */
 
 /**
  * Completa el bucle:
- * 
+ *
  * 1. Si la siguiente letra que ha introducido el usuario es la misma letra que la que tocaría en la palabra del día, mostramos por consola : "la letra {letra} es correcta"
  * 2. Si la siguiente letra que ha introducido el usuario NO esta en la palabra del dia, mostramos por consola : "le letra {letra} NO es correcta"
  * 3. Si la siguiente letra que ha introducido el usuario se encuentra en la palabra del dia, PERO no es la posición correcta, mostrar por consola: "la letra {letra} es correcta, PERO se encuentra en una posición diferente"
  */
 
-let palabraDelDia= "bucle";
+let palabraDelDia = "bucle";
 let palabraIntroducida = "bueno";
 
-for (let i=0;i<palabraDelDia.length;i++) {
+for (let i = 0; i < palabraDelDia.length; i++) {
+  let siguienteLetraCorrecta = palabraDelDia[i];
+  let siguienteLetraIntroducida = palabraIntroducida[i];
 
-    let siguienteLetraCorrecta = palabraDelDia[i];
-    let siguienteLetraIntroducida = palabraIntroducida[i];
-
-    // Tu código a partir de aquí, dentro del bucle
+  // Tu código a partir de aquí, dentro del bucle
+  if (siguienteLetraCorrecta === palabraDelDia[i]) {
+    console.log(`la plabra ${siguienteLetraCorrecta} es correcta`);
+  } else if (siguienteLetraCorrecta === siguienteLetraIntroducida) {
+    `La letra ${siguienteLetraIntroducida} no es correcto`;
+  } else {
+    console.log("no es correcto");
+  }
 }
 
 // Resultado esperado:
 
 /**
- * "La letra b es correcta"
+ *
  * "La letra u es correcta"
- * "La letra e es correcta, pero se encuentra en una posición diferente"
+ *
  * "La letra n NO es correcta"
  * "LA letra o NO es correcta"
  */
